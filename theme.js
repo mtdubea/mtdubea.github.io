b67cfa8f3490cbdb6e1a344b81fceaed2e28e16b
Matthew Dubea <mtdubea@gmail.com>
2025-06-24 17:17:02 -0500

    gallery.hyml and gallery.js and style.css
    gallery.hyml and gallery.js and style.css

--- theme.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("themeToggle");
-  const current = localStorage.getItem("theme");
-  if (current === "dark") document.body.classList.add("dark");
-  toggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    localStorage.setItem("theme", document.body.classList.contains("dark") ? "dark" : "light");
-  });
-});
