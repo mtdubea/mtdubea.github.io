4824e0768bf7b3562d2d242abcaf09abd9152244
Matthew Dubea <mtdubea@gmail.com>
2026-05-02 23:17:13 -0500

    Add mechatronics enhancement bridge script

---
+++ mechatronics/theme.js
@@ -0,0 +1,7 @@
+// Mechatronics microsite enhancement bridge
+(function () {
+  const script = document.createElement("script");
+  script.src = "../theme.js";
+  script.defer = true;
+  document.head.appendChild(script);
+})();
